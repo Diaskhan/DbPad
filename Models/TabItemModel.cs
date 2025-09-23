bf79afe2a1fe34cc2a354bc10f3d7d6ac5092158
Diaskhan <Diaskhan@yandex.kz>
2025-09-24 02:41:16 +0500

    made it tabs

--- Models/TabItemModel.cs
+++
@@ -1,8 +0,0 @@
-namespace DbPad.Models
-{
-    public class TabItemModel
-    {
-        public string Text1 { get; set; } = "Tab1";
-        public string Text2 { get; set; } = string.Empty;
-    }
-}
---
+++ ViewModels/TabItemModel.cs
@@ -0,0 +1,10 @@
+namespace DbPad.ViewModels
+{
+    public class TabItemModel
+    {
+
+        public string TabCaption { get; set; } = "Tab1";
+        public string Text1 { get; set; } = "Text1";
+        public string Text2 { get; set; } = "Text2";
+    }
+}
