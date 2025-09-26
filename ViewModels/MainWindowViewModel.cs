76c9d495cbc85ee9cf8aaef1b4cfa02e0e33faed
Diaskhan <Diaskhan@yandex.kz>
2025-09-26 16:05:11 +0500

    finnaly tabs are creating and focusing and sql executing

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using DbPad.Models;
 using DynamicData;
 using Microsoft.Data.SqlClient;
+using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -11,9 +14,21 @@ namespace DbPad.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         public ObservableCollection<Node> Nodes { get; set; }
-        public ObservableCollection<TabItemModel> Tabs { get; }
+        public ObservableCollection<TabItemModel> Tabs { get; } = new();
+
+        private TabItemModel? _selectedTab;
+        public TabItemModel? SelectedTab
+        {
+            get => _selectedTab;
+            set => this.RaiseAndSetIfChanged(ref _selectedTab, value);
+        }
+
+
         public ICommand AddTabCommand { get; }
         public RelayCommand AddConnectionCommand { get; }
+
+        public ICommand ExecuteSQLCommand { get; }
+
 
 
         #region COMANDS FOR CONTEXT MENU
@@ -33,11 +48,63 @@ namespace DbPad.ViewModels
 
             AddTabCommand = new RelayCommand(AddTab);
             AddConnectionCommand = new RelayCommand(async (parameter) => await AddConnectionAsync(null));
+            ExecuteSQLCommand = new RelayCommand(async (parameter) => await ExecuteSQLAsync(parameter));
+
 
             Select1000Command = new RelayCommand(Select1000);
             EditDataCommand = new RelayCommand(EditData);
             DesignTableCommand = new RelayCommand(DesignTable);
         }
+
+        private async Task ExecuteSQLAsync(object? parameter)
+        {
+            if (_selectedTab == null || string.IsNullOrWhiteSpace(_selectedTab.Text1))
+            {
+                return;
+            }
+
+
+            try
+            {
+                var connectionString = "Server=.\\sqlexpress;Trusted_Connection=True;TrustServerCertificate=True;";
+
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+                    //connection.ChangeDatabase("master");
+
+                    using (var cmd = new SqlCommand(_selectedTab.Text1, connection))
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        var results = new System.Text.StringBuilder();
+
+                        // Заголовки колонок
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            results.Append(reader.GetName(i)).Append("\t");
+                        }
+                        results.AppendLine();
+
+                        // Данные
+                        while (await reader.ReadAsync())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                results.Append(reader[i]?.ToString()).Append("\t");
+                            }
+                            results.AppendLine();
+                        }
+
+                        _selectedTab.Text2 = results.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _selectedTab.Text2 = "Ошибка выполнения: " + ex.Message;
+            }
+        }
+
 
         private void AddTab(object? parameter)
         {
@@ -91,6 +158,7 @@ namespace DbPad.ViewModels
                 Text1 = $"SELECT TOP 1000 * FROM [{selectedNode?.Title}];",
                 Text2 = "-- Results will be displayed here"
             });
+            SelectedTab = Tabs.LastOrDefault();
 
         }
 
--- ViewModels/TabItemModel.cs
+++ ViewModels/TabItemModel.cs
@@ -1,10 +1,40 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace DbPad.ViewModels
 {
-    public class TabItemModel
+    public class TabItemModel : INotifyPropertyChanged
     {
+        private string _tabCaption = "Tab1";
+        private string _text1 = "Text1";
+        private string _text2 = "Text2";
 
-        public string TabCaption { get; set; } = "Tab1";
-        public string Text1 { get; set; } = "Text1";
-        public string Text2 { get; set; } = "Text2";
+        public string TabCaption
+        {
+            get => _tabCaption;
+            set => SetField(ref _tabCaption, value);
+        }
+
+        public string Text1
+        {
+            get => _text1;
+            set => SetField(ref _text1, value);
+        }
+
+        public string Text2
+        {
+            get => _text2;
+            set => SetField(ref _text2, value);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
     }
 }
