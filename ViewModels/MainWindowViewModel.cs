36975bbf6df4efde882f024cd7d2c5846a0e82c6
Diaskhan <Diaskhan@yandex.kz>
2025-09-27 15:12:06 +0500

    added conections

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using DbPad.Adapter.MsSql;
 using DbPad.Common.Models;
 using DynamicData;
-using Microsoft.Data.SqlClient;
 using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -32,7 +34,7 @@ namespace DbPad.ViewModels
 
 
 
-        #region COMANDS FOR CONTEXT MENU
+        #region КОМАНДЫ ДЛЯ КОНТЕКСТНОГО МЕНЮ
         public ICommand Select1000Command { get; set; }
         public ICommand EditDataCommand { get; set; }
         public ICommand DesignTableCommand { get; set; }
@@ -43,14 +45,48 @@ namespace DbPad.ViewModels
             Tabs = new ObservableCollection<TabItemModel>();
 
             AddTabCommand = new RelayCommand(AddTab);
-            AddConnectionCommand = new RelayCommand(async (parameter) => await AddConnectionAsync(null));
+            // Эта команда теперь будет обрабатывать параметр, представляющий узел подключения
+            AddConnectionCommand = new RelayCommand(async (parameter) => await AddConnectionAsync(parameter));
 
             Select1000Command = new RelayCommand(Select1000);
             EditDataCommand = new RelayCommand(EditData);
             DesignTableCommand = new RelayCommand(DesignTable);
+            
+            // Вызов метода для загрузки подключений при запуске
+            LoadConnectionsOnStartup();
         }
 
+        // Метод для загрузки подключений из файла
+        private void LoadConnectionsOnStartup()
+        {
+            // Путь к файлу connections.json в директории запуска приложения
+            string filePath = Path.Combine(AppContext.BaseDirectory, "connections.json");
 
+            if (File.Exists(filePath))
+            {
+     
+                    string jsonString = File.ReadAllText(filePath);
+                    var rootList = JsonSerializer.Deserialize<List<ConnectionFileRoot>>(jsonString);
+
+                    if (rootList != null && rootList.Any())
+                    {
+                        var firstRoot = rootList.First();
+                        if (firstRoot?.MssqlConnections != null)
+                        {
+                            foreach (var info in firstRoot.MssqlConnections)
+                            {
+
+                                Nodes.Add(new Node(info.DisplayName, info.ConnectionString, NodeType.Connection));
+                            }
+                        }
+                    }
+
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine($"Файл подключений не найден по адресу: {filePath}");
+            }
+        }
 
 
         private void AddTab(object? parameter)
@@ -58,12 +94,54 @@ namespace DbPad.ViewModels
             Tabs.Add(new TabItemModel());
         }
 
+        // Обновлен для работы с узлами подключения
         private async Task AddConnectionAsync(object? parameter)
         {
-            var nodes = await MsSqlAdapter.LoadDatabasesAndTablesAsync(MsSqlAdapter.connectionString);
+            string? connectionString = null;
+            Node? parentConnectionNode = null;
 
-            Nodes.Clear();
-            Nodes.AddRange(nodes);
+            if (parameter is Node node && node.Type == NodeType.Connection)
+            {
+                connectionString = node.Database; // В этом контексте Node.Database хранит строку подключения для NodeType.Connection
+                parentConnectionNode = node;
+            }
+            // Если сюда попадает MsSqlAdapter.connectionString, это значит, что вызывается команда без выбранного узла
+            else if (parameter is string paramString && !string.IsNullOrEmpty(paramString))
+            {
+                connectionString = paramString;
+            }
+            else
+            {
+                // Fallback или запрос у пользователя
+                connectionString = MsSqlAdapter.connectionString; // Исходный fallback для примера
+            }
+
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                var nodes = await MsSqlAdapter.LoadDatabasesAndTablesAsync(connectionString);
+
+                if (parentConnectionNode != null)
+                {
+                    // Очищаем существующие под-узлы и добавляем новые, если узел уже существует
+                    parentConnectionNode.SubNodes?.Clear();
+                    if (parentConnectionNode.SubNodes == null)
+                    {
+                        parentConnectionNode.SubNodes = new ObservableCollection<Node>();
+                    }
+                    parentConnectionNode.SubNodes.AddRange(nodes);
+                }
+                else
+                {
+                    // Если узел родительского подключения не определен, возможно, вы хотите добавить его как новый корневой элемент
+                    // или обновить существующий, если это нежелательное поведение, его можно изменить
+                    Nodes.Clear(); // Рассмотрите, всегда ли это очистка желательна, возможно, вы хотите добавить к определенному родителю.
+                    Nodes.AddRange(nodes);
+                }
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Строка подключения не предоставлена для AddConnectionAsync.");
+            }
         }
 
         private void Select1000(object parameter)
@@ -73,7 +151,7 @@ namespace DbPad.ViewModels
             {
                 TabCaption = $"FROM {selectedNode?.Title}",
                 Query = MsSqlAdapter.Select1000Query(selectedNode?.Title ?? ""),
-                Results = "-- Results will be displayed here",
+                Results = "-- Результаты будут здесь",
                 Database = selectedNode?.Database ?? ""
             });
             SelectedTab = Tabs.LastOrDefault();
